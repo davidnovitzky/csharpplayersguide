535cf8b1728c4cc111132abcf3f19fb3fb186bd3
davidnovitzky <deivido.navicko@gmail.com>
2025-05-26 20:26:55 +0300

    Update level_12_the_laws_of_freach.cs

--- level_12_the_laws_of_freach.cs
+++ level_12_the_laws_of_freach.cs
@@ -23,23 +23,22 @@ float average = total / arrayValues.Length;
 Console.WriteLine($"The average from the list: {average.ToString("F2")}");
 Console.ReadLine();
 
-/*int[] arrayValues = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+/*int[] arrayValues = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 }; //initializes 9 values inside if the array
 
-int currentSmallest = int.MaxValue;
-float total = 0;
+int currentSmallest = int.MaxValue; // variable will hold the smallest number each pass, current value is the largest available
+float total = 0; // initialized float variable will hold the average of the values inside the array
 
-foreach (int value in arrayValues)
+foreach (int value in arrayValues) // for every loop pass assign the value from the array to the new "value" int variable
 {
-    if (value < currentSmallest)
+    if (value < currentSmallest) // conditional statement if the value is smaller than the currentSmallest is true then..
     {
-        currentSmallest = value;
-        
+        currentSmallest = value; // then the new value is assigned to the currentSmallest variable        
     }
-        total += value;
+        total += value; // this adds each value to the total float variable to be used in the formula to calculate the average, it cant be inside the if statement because the condition doesnt work for this
 }
 
-float average = total / arrayValues.Length;
+float average = total / arrayValues.Length; // the calculated total of the arrays values divided by the total length of the array is assigned as the average to a new variable
 
-Console.WriteLine($"Current smallest value: {currentSmallest}");
-Console.WriteLine($"The average from the list: {average.ToString("F2")}");
+Console.WriteLine($"Current smallest value: {currentSmallest}"); // prints the left over smallest value from the array
+Console.WriteLine($"The average from the list: {average.ToString("F2")}"); // prints the total values average with only two decimals
 Console.ReadLine();*/
