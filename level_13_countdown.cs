ef34ccdd38f3cb0b7b714ff78600a56d99c497cc
davidnovitzky <deivido.navicko@gmail.com>
2025-05-30 16:33:02 +0300

    Update level_13_countdown.cs

--- level_13_countdown.cs
+++ level_13_countdown.cs
@@ -1,19 +1,24 @@
-Challenge(10);
-int Challenge(int number)
-{
-    if (number == 0) return 1;
-    Console.WriteLine(number);
-    return number * Challenge(number - 1);
-}
+int number = Factorial(4);
+Console.WriteLine(number);
 Console.ReadLine();
 
+int Factorial(int number)
+{
+    if (number == 1) return 1;
+    return number * Factorial(number - 1);
+}
 
-//int number = Factorial(4);
-//Console.WriteLine(number);
-//Console.ReadLine();
+/* OBJECTIVES
+ * Write code that counts down from 10 to 1 using a recursive method
+ * You have to have a base case that ends the recursion
+ * Everytime you call the method you must be getting closer to that base case
+ */
 
-//int Factorial(int number)
+//Challenge(10);
+//int Challenge(int number)
 //{
-//    if (number == 1) return 1;
-//    return number * Factorial(number - 1);
+//    if (number == 0) return 1;
+//    Console.WriteLine(number);
+//    return number * Challenge(number - 1);
 //}
+//Console.ReadLine();
