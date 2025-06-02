8651babaff4f838f014b8d9091b5b36c63fce76c
davidnovitzky <deivido.navicko@gmail.com>
2025-06-02 19:43:01 +0300

    Update manticore.cs

--- manticore.cs
+++ manticore.cs
@@ -5,9 +5,9 @@
 // Display the status for the round, city health and manticore health
 // Display the amount of damage expected on a hit.
 // Get the input of a number from the player for the cannon range
-// Display the outcome if it fell short or overshot or if it hit.
+// Display the outcome if the attack fell short, overshot or if it hit.
 // Deal damage to the Manticore if it was a hit.
-// Deal damage to the city if the Manticore is still alive.
+// Deal damage to the city if the Manticore is still alive every round.
 // Go on to the next round until the city or manticore is destroyed (loop)
 // Display outcome of the game if manticore or city health is = 0
 // ------------------------------- METHODS --------------------------------
@@ -15,5 +15,3 @@
 // 2.Tells the player if they fell short, overshot, or hit their target.
 // 3.Displays the status of the game, including the round number, and health of city and Manticore.
 // 4.Calculate how much damage will be done depending on the current round.
-// 5.Gets a number from the user, asking the prompt supplied by 'text'.
-// 6.Gets a number from the user and ensures it is in the given range.
