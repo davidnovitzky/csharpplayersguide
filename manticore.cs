ac9004e60864a048ebd5ef4eff3bb9c03883f362
davidnovitzky <deivido.navicko@gmail.com>
2025-06-02 09:34:52 +0300

    Create manticore.cs

---
+++ manticore.cs
@@ -0,0 +1,19 @@
+// Setup the game with health and round
+// Get the starting distance for the Manticore.
+// Second player turn
+// Run the game until the city is destroyed or the Manticore is destroyed.
+// Display the status for the round, city health and manticore health
+// Display the amount of damage expected on a hit.
+// Get a number from the player for the cannon range
+// Display the outcome of the number.
+// Deal damage to the Manticore if it was a hit.
+// Deal damage to the city if the Manticore is still alive.
+// Go on to the next round until the city or manticore is destroyed
+// Display outcome of the game if manticore or city health is = 0
+// ------------------------------- METHODS --------------------------------
+// 1.Display the outcome of the game, win or lose
+// 2.Tells the player if they fell short, overshot, or hit their target.
+// 3.Displays the status of the game, including the round number, and health of city and Manticore.
+// 4.Computes how much damage will be done depending on the current round.
+// 5.Gets a number from the user, asking the prompt supplied by 'text'.
+// 6.Gets a number from the user and ensures it is in the given range.
