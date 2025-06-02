20227cba64a2438375afd1c0bcd669444943ce33
davidnovitzky <deivido.navicko@gmail.com>
2025-06-02 21:54:17 +0300

    Create level_3_Consolas_and_Telim.cs

---
+++ level_3_Consolas_and_Telim.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string name = Console.ReadLine();
+Console.WriteLine("Noted: "+ name + " got bread.");
+Console.ReadLine();
