4b90e4ea4f604ee25b8052e75bb7744dd2cdc8fb
davidnovitzky <deivido.navicko@gmail.com>
2025-06-04 09:08:52 +0300

    Update level_13_taking_a_number.cs

--- level_13_taking_a_number.cs
+++ level_13_taking_a_number.cs
@@ -1,1 +1,5 @@
-Console.WriteLine("Hello");
+// Create a program that asks a user to input a number from 0 to 100
+// Then the other user has to guess the number
+// Display if the users answer is too low or too high
+// Use a method inside another method
+// * Objective is to write a method to call it several times so you dont have to write the same code twice*
