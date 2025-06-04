b756269104b7feb19080e07ac9417410b2fdc331
davidnovitzky <deivido.navicko@gmail.com>
2025-06-04 09:09:11 +0300

    Delete level_3_Consolas_and_Telim.cs

--- level_3_Consolas_and_Telim.cs
+++
@@ -1,5 +0,0 @@
-Console.WriteLine("Bread is ready.");
-Console.WriteLine("Who is the bread for?");
-string name = Console.ReadLine();
-Console.WriteLine("Noted: "+ name + " got bread.");
-Console.ReadLine();
