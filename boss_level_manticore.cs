c02f87323f951ffe77507d79e4896361d4513375
davidnovitzky <deivido.navicko@gmail.com>
2025-06-05 23:28:35 +0300

    Update boss_level_manticore.cs

--- boss_level_manticore.cs
+++ boss_level_manticore.cs
@@ -1,21 +1,3 @@
-// Setup the game with health for manticore and city then rounds
-// Get the starting distance for the Manticore from the first player and clear the screen after.
-// Second player turn
-// Run the game in a loop until the city or the Manticore is destroyed.
-// Display the status for the round, city health and manticore health
-// Display the amount of damage expected on a hit.
-// Get the input of a number from the player for the cannon range
-// Display the outcome if the attack fell short, overshot or if it hit.
-// Deal damage to the Manticore if it was a hit.
-// Deal damage to the city if the Manticore is still alive every round.
-// Go on to the next round until the city or manticore is destroyed (loop)
-// Display outcome of the game if manticore or city health is = 0
-// ------------------------------- METHODS --------------------------------
-// 1.Display the outcome of the game, win or lose
-// 2.Tells the player if they fell short, overshot, or hit their target.
-// 3.Displays the status of the game, including the round number, and health of city and Manticore.
-// 4.Calculate how much damage will be done depending on the current round.
-
 const int startingManticoreHealth = 10;
 const int startingCityHealth = 15;
 int manticoreHealth = 10;
