bd161e7b8b13d94d09521760e92680d78c0a04e8
davidnovitzky <deivido.navicko@gmail.com>
2025-06-21 23:01:55 +0300

    Create level_17_Simula’s Soup.cs

---
+++ level_17_Simula’s Soup.cs
@@ -0,0 +1,13 @@
+// Define an enum for a food type with values: soup, stew and gumbo
+// Define a separate enum for ingredients: mushrooms, chicken, carrots, potatoes
+// Define a separate enum for seasoning: spicy, salty, sweet
+
+// Make a tuple variable to represent a soup composed of the three above enumeration types.
+
+// Let the user pick a type, main ingredient, and seasoning from the allowed choices and fill the tuple with the results.
+// Hint: You could give the user a menu to pick from or simply compare the
+// user’s text input against specific strings to determine which enumeration value represents their choice.
+
+// When done, display the contents of the soup tuple variable in a format like “Sweet Chicken Gumbo.”
+// You don’t need to convert the enumeration value back to a string. Simply displaying an
+// enumeration value with Write or WriteLine will display the name of the enumeration value.)
