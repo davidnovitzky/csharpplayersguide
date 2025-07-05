5e67a1bf18a5043311e0e6e9d8c4e05d8995108c
davidnovitzky <deivido.navicko@gmail.com>
2025-07-05 22:49:32 +0300

    Update level_24_The Card.cs

--- level_24_The Card.cs
+++ level_24_The Card.cs
@@ -1,3 +1,9 @@
+// Define enumerations for card colors and card ranks.
+// Define a Card class to represent a card with a color and a rank
+// Add properties or methods that tell you if a card is a number or symbol card (the equivalent of a face card)
+// Create a main method that will create a card instance for the whole deck
+
+
 CardColor[] cardColors = new[] { CardColor.Red, CardColor.Green, CardColor.Blue, CardColor.Yellow };
 CardRank[] cardRanks = new[] { CardRank.One, CardRank.Two, CardRank.Three, CardRank.Four, CardRank.Five, CardRank.Six, CardRank.Seven, CardRank.Eight, CardRank.Nine, CardRank.Ten, CardRank.Jack, CardRank.Queen, CardRank.King, CardRank.Ace };
 
@@ -20,7 +26,7 @@ public class Card
         Color = color;
         Rank = rank;
     }
-
+    // if Rank property == CardRank.Ace it will return true for the IsFace property
     public bool IsFace => Rank == CardRank.Ace || Rank == CardRank.King || Rank == CardRank.Queen || Rank == CardRank.Jack;
     public bool IsNumber => !IsFace;
 }
