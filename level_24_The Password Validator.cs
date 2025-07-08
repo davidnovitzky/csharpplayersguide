5f1ffbd8fe00e8ddb290435ee4222ad4d1b4b165
davidnovitzky <deivido.navicko@gmail.com>
2025-07-08 11:59:22 +0300

    Create level_24_The Password Validator.cs

---
+++ level_24_The Password Validator.cs
@@ -0,0 +1,64 @@
+PasswordValidator passValid = new PasswordValidator();
+
+while (true)
+{
+    Console.Write("Enter a password: ");
+    string? passwordInput = Console.ReadLine();
+
+    if (passValid.IsValid(passwordInput!) == true)
+    {
+        Console.WriteLine("The password is valid");
+    }
+    else
+    {
+        Console.WriteLine("The password is invalid");
+    }
+}
+public class PasswordValidator
+{
+    public bool IsValid(string password)
+    {
+        if (!UpperCase(password) == true) return false;
+        if (!LowerCase(password) == true) return false;
+        if (!IsNumber(password) == true) return false;
+        if (password.Length < 6) return false;
+        if (password.Length > 13) return false;
+        if (password.Contains('T')) return false;
+        if (password.Contains('&')) return false;
+
+        return true;
+    }
+    public bool UpperCase(string password)
+    {
+        foreach (char letter in password)
+        {
+            if (char.IsUpper(letter))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public bool LowerCase(string password)
+    {
+        foreach (char letter in password)
+        {
+            if (char.IsLower(letter))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public bool IsNumber(string password)
+    {
+        foreach (char letter in password)
+        {
+            if (char.IsDigit(letter))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
